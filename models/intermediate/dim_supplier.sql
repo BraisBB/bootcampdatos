64dc722de826f538da029cf03ce3b46b75437028
Samuel Aguiar <samuel.aguiar@altia.es>
2024-07-03 06:40:08 +0000

    dim_supplier y correcciones de errores

---
+++ models/intermediate/dim_supplier.sql
@@ -0,0 +1,14 @@
+WITH supplier_transformation AS (
+    SELECT 
+        s.s_suppkey,
+        s.s_name AS s_suppname,
+        s.s_address,
+        n.n_name AS s_suppnationname,
+        r.r_regionkey AS s_suppregionname,
+        s_phone,
+        s_acctbal
+    FROM {{ ref('stg_supplier') }} s
+    LEFT JOIN {{ ref('stg_nation') }} n ON s.s_nationkey = n.n_nationkey
+    LEFT JOIN {{ ref('stg_region') }} r ON n.n_regionkey = r.r_regionkey
+)
+SELECT * FROM supplier_transformation;
