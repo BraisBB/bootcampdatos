2b548187fcea814cf1f4c1cce7d4a4bbadfa20ea
Adrian Lopez <adrian.lopez.e@altia.es>
2024-07-04 09:14:30 +0000

    fdslosf

--- models/analytics/aggr_sales_customer.sql
+++ models/analytics/aggr_sales_customer.sql
@@ -6,45 +6,8 @@
 ) }}
 
 WITH sales_data AS (
-    SELECT 
-        l.l_orderkey,
-        o.o_custkey,
-        l.l_partkey,
-        l.l_quantity,
-        l.l_extendedprice,
-        l.l_discount,
-        l.l_tax,
-        l.tipo_operacion,
-        o.clerk,
-        o.o_orderdate AS fecha_pedido,
-        l.l_shipdate AS fecha_envio,
-        l.l_receiptdate AS fecha_recepcion,
-        o.o_totalprice AS totalprice,
-        e.id_evento,
-        d.tienda,
-        d.pais AS pais_tienda,
-        er.tipo_cambio AS tipo_cambio_tienda,
-        l.l_extendedprice * (1 - l.l_discount) + l.l_tax AS totalprice_usd,
-        (l.l_extendedprice * (1 - l.l_discount) + l.l_tax) * er.tipo_cambio AS totalprice_local_store,
-        o.o_totalprice * er.tipo_cambio AS totalprice_local_customer,
-        o.o_orderdate AS orderdate_UTC,
-        DATEADD('SECOND', UNIFORM(0, 86400, RANDOM()),  o.o_orderdate) AS orderdate_local_store, -- Simulación de la conversión de zona horaria
-        DATEADD('SECOND', UNIFORM(0, 86400, RANDOM()),  o.o_orderdate) AS orderdate_local_customer, -- Simulación de la conversión de zona horaria
-        l.l_shipdate AS shipdate_UTC,
-        DATEADD('SECOND', UNIFORM(0, 86400, RANDOM()), l.l_shipdate) as shipdate_local,
-        l.l_commitdate AS commitdate_UTC,
-        DATEADD(hour, 5, l.l_commitdate) AS commitdate_local,
-        l.l_receiptdate AS receiptdate_UTC,
-        DATEADD('SECOND', UNIFORM(0, 86400, RANDOM()), l.l_receiptdate) as receiptdate_local,
-        CASE 
-            WHEN DATEDIFF(day, l.l_shipdate, l.l_receiptdate) <= 10 THEN 'En plazo'
-            WHEN DATEDIFF(day, l.l_shipdate, l.l_receiptdate) <= 20 THEN 'Retraso moderado'
-            ELSE 'Retraso considerable'
-        END AS plazo_entrega
-    FROM {{ ref('fact_sales') }} fs
-    JOIN {{ ref('dim_store') }} d ON fs.tienda = d.tienda
-    JOIN {{ ref('dim_event') }} e ON fs.id_evento = e.id_evento
-    LEFT JOIN {{ ref('exchange_rates') }} er ON d.pais = er.pais
+    SELECT *
+    FROM {{ ref('fact_sales') }} 
 )
 
 SELECT 
@@ -55,4 +18,4 @@ SELECT
     SUM(totalprice_usd) AS total_sales_usd,
     SUM(totalprice_local_customer) AS total_sales_local
 FROM sales_data
-GROUP BY o_custkey, fecha_pedido::date;
+GROUP BY o_custkey, fecha_pedido::date
